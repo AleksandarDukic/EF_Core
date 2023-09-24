65c6a8bf563f433c3b732e840449fd53b24cc469
Kiki <aleksandar.z.dukic@gmail.com>
2023-09-24 13:20:23 +0200

    Chapter 5 done

---
+++ Chapter_2/PublisherData/Migrations/20230924004350_seed.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PublisherData.Migrations
+{
+    /// <inheritdoc />
+    public partial class seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[]
+                {
+                    "Id", "FirstName", "LastName"
+                },
+                values: new object[,]
+                {
+                    {1, "Rhoda", "Lerman" },
+                    {2, "Pear", "Zika" }
+
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1);
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
